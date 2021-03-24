bf55aae4023c3ea7f144afab792648a24f52d277
bscalera <BenjaminScalera@gmail.com>
2021-03-24 02:49:31 -0400

    Added converted code and a comment
    Added code converted by the program at https://hwiegman.home.xs4all.nl/python2vb.html , and a comment in the program.vb file

---
+++ vbnetpython/ClassConvertedCode.vb
@@ -0,0 +1,235 @@
+﻿Public Class ClassConvertedCode
+    Public Sub New()
+        'Code converted by the program at https://hwiegman.home.xs4all.nl/python2vb.html
+
+
+        '        Private Python2VBText As New TextBox
+        'Private now As Integer
+
+        '' import pandas as pd
+        '' import datetime
+        '' import numpy as np
+        'Public Sub Routine2
+        '    Python2VBText.Text = Python2VBText.Text +"running...".ToString + vbNewLine 
+        'Python2VBText.MultiLine = True
+        'Python2VBText.Width = 200
+        'Python2VBText.Height = 100
+        'Form1.Controls.Add(Python2VBText)
+        'Dim csvFile As String
+        'csvFile = "JMOF_N_KOREA091920"
+        'now = datetime.datetime.now()
+        'End Sub
+        'Public Sub akaTagGenerator(aliasList As Object)
+        '    Dim akaTags As String
+        '    akaTags = ""
+        '        Dim akaTag As String
+        '    for each_aka= 0 To aliasList.Length - 1
+
+
+        '        akaTag = "<aka>" + vbNewLine + 
+        '"<alttype></alttype>" + vbNewLine + 
+        '"<altname>"
+        '            <firstname></firstname>
+        '            <middlename></middlename>
+        '            <lastname></lastname>
+        '            <remarks></remarks>
+
+
+        '    </aka>"<aka>" + vbNewLine + 
+        '"<alttype></alttype>" + vbNewLine + 
+        '"<altname>""akaTags = akaTags+akaTag" + vbNewLine + 
+        '"Return akaTags" + vbNewLine + 
+        '"def convert_row(dictVals, akaChildTags):" + vbNewLine + 
+        '"tempStr ="
+        '  Next
+        '  <party>
+        '    <info>
+
+
+        '    <uid>"<aka>" + vbNewLine + 
+        '"<alttype></alttype>" + vbNewLine + 
+        '"<altname>""akaTags = akaTags+akaTag" + vbNewLine + 
+        '"Return akaTags" + vbNewLine + 
+        '"def convert_row(dictVals, akaChildTags):" + vbNewLine + 
+        '"tempStr =""<fullname>"
+        '      <firstname></firstname>
+        '      <middlename></middlename>
+        '      <lastname></lastname>
+        '      <title></title>
+
+
+        '    <program>"<aka>" + vbNewLine + 
+        '"<alttype></alttype>" + vbNewLine + 
+        '"<altname>""akaTags = akaTags+akaTag" + vbNewLine + 
+        '"Return akaTags" + vbNewLine + 
+        '"def convert_row(dictVals, akaChildTags):" + vbNewLine + 
+        '"tempStr =""<fullname>""<partytype>"
+
+
+        '    <vesstype>"<aka>" + vbNewLine + 
+        '"<alttype></alttype>" + vbNewLine + 
+        '"<altname>""akaTags = akaTags+akaTag" + vbNewLine + 
+        '"Return akaTags" + vbNewLine + 
+        '"def convert_row(dictVals, akaChildTags):" + vbNewLine + 
+        '"tempStr =""<fullname>""<partytype>""<tonnage></tonnage>" + vbNewLine + 
+        '"<grt></grt>" + vbNewLine + 
+        '"<vessflag></vessflag>" + vbNewLine + 
+        '"<vessowner></vessowner>" + vbNewLine + 
+        '"<dob></dob>" + vbNewLine + 
+        '"<sex></sex>" + vbNewLine + 
+        '"<height></height>" + vbNewLine + 
+        '"<weight></weight>" + vbNewLine + 
+        '"<build></build>" + vbNewLine + 
+        '"<eyes></eyes>" + vbNewLine + 
+        '"<hair></hair>" + vbNewLine + 
+        '"<complexion></complexion>" + vbNewLine + 
+        '"<race></race>" + vbNewLine + 
+        '"<country>"
+        '      <remarks></remarks>
+        '      <entitytype></entitytype>
+        '      <listtype></listtype>
+        '      <callsign></callsign>
+        '    </info>
+
+
+        '    <akas>"<aka>" + vbNewLine + 
+        '"<alttype></alttype>" + vbNewLine + 
+        '"<altname>""akaTags = akaTags+akaTag" + vbNewLine + 
+        '"Return akaTags" + vbNewLine + 
+        '"def convert_row(dictVals, akaChildTags):" + vbNewLine + 
+        '"tempStr =""<fullname>""<partytype>""<tonnage></tonnage>" + vbNewLine + 
+        '"<grt></grt>" + vbNewLine + 
+        '"<vessflag></vessflag>" + vbNewLine + 
+        '"<vessowner></vessowner>" + vbNewLine + 
+        '"<dob></dob>" + vbNewLine + 
+        '"<sex></sex>" + vbNewLine + 
+        '"<height></height>" + vbNewLine + 
+        '"<weight></weight>" + vbNewLine + 
+        '"<build></build>" + vbNewLine + 
+        '"<eyes></eyes>" + vbNewLine + 
+        '"<hair></hair>" + vbNewLine + 
+        '"<complexion></complexion>" + vbNewLine + 
+        '"<race></race>" + vbNewLine + 
+        '"<country>""</akas>" + vbNewLine + 
+        '"<addresses>" + vbNewLine + 
+        '"<address>" + vbNewLine + 
+        '"<addr1>"
+        '        <addr2></addr2>
+        '        <addr3></addr3>
+        '        <addr4></addr4>
+
+
+        '    <city>"<aka>" + vbNewLine + 
+        '"<alttype></alttype>" + vbNewLine + 
+        '"<altname>""akaTags = akaTags+akaTag" + vbNewLine + 
+        '"Return akaTags" + vbNewLine + 
+        '"def convert_row(dictVals, akaChildTags):" + vbNewLine + 
+        '"tempStr =""<fullname>""<partytype>""<tonnage></tonnage>" + vbNewLine + 
+        '"<grt></grt>" + vbNewLine + 
+        '"<vessflag></vessflag>" + vbNewLine + 
+        '"<vessowner></vessowner>" + vbNewLine + 
+        '"<dob></dob>" + vbNewLine + 
+        '"<sex></sex>" + vbNewLine + 
+        '"<height></height>" + vbNewLine + 
+        '"<weight></weight>" + vbNewLine + 
+        '"<build></build>" + vbNewLine + 
+        '"<eyes></eyes>" + vbNewLine + 
+        '"<hair></hair>" + vbNewLine + 
+        '"<complexion></complexion>" + vbNewLine + 
+        '"<race></race>" + vbNewLine + 
+        '"<country>""</akas>" + vbNewLine + 
+        '"<addresses>" + vbNewLine + 
+        '"<address>" + vbNewLine + 
+        '"<addr1>""<state></state>" + vbNewLine + 
+        '"<postalcode></postalcode>" + vbNewLine + 
+        '"<country>"
+        '        <remarks></remarks>
+        '      </address>
+        '    </addresses>
+        '    <dobs>
+
+
+        '    <dob>"<aka>" + vbNewLine + 
+        '"<alttype></alttype>" + vbNewLine + 
+        '"<altname>""akaTags = akaTags+akaTag" + vbNewLine + 
+        '"Return akaTags" + vbNewLine + 
+        '"def convert_row(dictVals, akaChildTags):" + vbNewLine + 
+        '"tempStr =""<fullname>""<partytype>""<tonnage></tonnage>" + vbNewLine + 
+        '"<grt></grt>" + vbNewLine + 
+        '"<vessflag></vessflag>" + vbNewLine + 
+        '"<vessowner></vessowner>" + vbNewLine + 
+        '"<dob></dob>" + vbNewLine + 
+        '"<sex></sex>" + vbNewLine + 
+        '"<height></height>" + vbNewLine + 
+        '"<weight></weight>" + vbNewLine + 
+        '"<build></build>" + vbNewLine + 
+        '"<eyes></eyes>" + vbNewLine + 
+        '"<hair></hair>" + vbNewLine + 
+        '"<complexion></complexion>" + vbNewLine + 
+        '"<race></race>" + vbNewLine + 
+        '"<country>""</akas>" + vbNewLine + 
+        '"<addresses>" + vbNewLine + 
+        '"<address>" + vbNewLine + 
+        '"<addr1>""<state></state>" + vbNewLine + 
+        '"<postalcode></postalcode>" + vbNewLine + 
+        '"<country>""</dobs>" + vbNewLine + 
+        '"<notes>" + vbNewLine + 
+        '"<note type="" value=\"
+        '    </notes>
+        '    <urls>
+        '      <url>
+        '        <address></address>
+        '        <description></description>
+        '      </url>
+        '    </urls>
+        '  </party>
+        '    ' 
+        '    '     return tempStr
+        '    ' 
+        '    ' csvDataO = pd.read_csv('C:\\Users\\ChaminiTirupathi\\Desktop\\Projects\\MUFG\\csvtoxml\\'+csvFile+'.csv')
+        '    ' csvData = csvDataO.replace(np.nan, '', regex=True)
+        '    ' 
+
+        '    '         ' xmlOutput = "<aka>" + vbNewLine + 
+        '"<alttype></alttype>" + vbNewLine + 
+        '"<altname>""akaTags = akaTags+akaTag" + vbNewLine + 
+        '"return akaTags" + vbNewLine + 
+        '"def convert_row(dictVals, akaChildTags):" + vbNewLine + 
+        '"tempStr =""<fullname>""<partytype>""<tonnage></tonnage>" + vbNewLine + 
+        '"<grt></grt>" + vbNewLine + 
+        '"<vessflag></vessflag>" + vbNewLine + 
+        '"<vessowner></vessowner>" + vbNewLine + 
+        '"<dob></dob>" + vbNewLine + 
+        '"<sex></sex>" + vbNewLine + 
+        '"<height></height>" + vbNewLine + 
+        '"<weight></weight>" + vbNewLine + 
+        '"<build></build>" + vbNewLine + 
+        '"<eyes></eyes>" + vbNewLine + 
+        '"<hair></hair>" + vbNewLine + 
+        '"<complexion></complexion>" + vbNewLine + 
+        '"<race></race>" + vbNewLine + 
+        '"<country>""</akas>" + vbNewLine + 
+        '"<addresses>" + vbNewLine + 
+        '"<address>" + vbNewLine + 
+        '"<addr1>""<state></state>" + vbNewLine + 
+        '"<postalcode></postalcode>" + vbNewLine + 
+        '"<country>""</dobs>" + vbNewLine + 
+        '"<notes>" + vbNewLine + 
+        '"<note type="" value=\""<parties>"
+        '    ' ' default empty values for each column/field
+        '    ' 
+        '    ' for i, row in csvData.iterrows()
+        '    '     row_dict = row.to_dict()
+        '    '     ' aka string generator
+        '    "     akaList = row_dict["Alias").split(sep="@")
+        '    '     akaTags = akaTagGenerator(aliasList=akaList)
+        '    '     print akaTags 
+        '    '     xmlStr = convert_row(row_dict,akaTags)
+        '    '     xmlOutput = xmlOutput + xmlStr
+        '    ' 
+        '    ' xmlOutput = xmlOutput+
+
+
+
+    End Sub
+End Class
--- vbnetpython/Program.vb
+++ vbnetpython/Program.vb
@@ -2,6 +2,7 @@ Imports System
 
 Module Program
     Sub Main(args As String())
+        'This is how a comment is written. - https://stackoverflow.com/questions/13477958/in-visual-basic-how-do-you-create-a-block-comment
         Console.WriteLine("Hello World!")
     End Sub
 End Module
