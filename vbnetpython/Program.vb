ed44a30a161c82b3867300411759901358c1b11b
bscalera <BenjaminScalera@gmail.com>
2021-03-24 04:28:51 -0400

    The program creates a text file.
    "How to: Create a File in Visual Basic" found at https://docs.microsoft.com/en-us/dotnet/visual-basic/developing-apps/programming/drives-directories-files/how-to-create-a-file

--- vbnetpython/Program.vb
+++ vbnetpython/Program.vb
@@ -1,8 +1,23 @@
 Imports System
+Imports System.IO
+Imports System.Text
 
 Module Program
     Sub Main(args As String())
         'This is how a comment is written. - https://stackoverflow.com/questions/13477958/in-visual-basic-how-do-you-create-a-block-comment
         Console.WriteLine("Hello World!")
+
+        'https://docs.microsoft.com/en-us/dotnet/visual-basic/developing-apps/programming/drives-directories-files/how-to-create-a-file
+        Dim path As String = "C:\Users\BenjaminScalera\Documents\GitHub\VBnet-python\vbnetpython\test.txt"
+
+        ' Create or overwrite the file.
+        Dim fs As FileStream = File.Create(path)
+
+        ' Add text to the file.
+        Dim info As Byte() = New UTF8Encoding(True).GetBytes("This is some text in the file.")
+        fs.Write(info, 0, info.Length)
+        fs.Close()
+
+
     End Sub
 End Module
